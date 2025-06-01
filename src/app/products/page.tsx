e833c3cc1ff9ff9e4b9dd6b09113df0d441e754c
DanielVerissimo1 <danielSantoss1300@gmail.com>
2025-06-01 20:36:49 -0300

    feat: realizando a atividade

---
+++ src/app/products/page.tsx
@@ -0,0 +1,21 @@
+
+const products = [
+  { name: "Camisa", price: 49.90 },
+  { name: "Tênis", price: 199.90 },
+  { name: "Relógio", price: 299.90 },
+];
+
+export default function ProductsPage() {
+  return (
+    <div>
+      <h1>Lista de Produtos</h1>
+      <ul>
+        {products.map((product, index) => (
+          <li key={index}>
+            <strong>{product.name}</strong> - R$ {product.price.toFixed(2)}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
