a18f52af7628e41d172bba2b466d9b203fce6872
DanielVerissimo1 <danielSantoss1300@gmail.com>
2025-06-25 19:01:51 -0300

    feat: Map points

--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -1,4 +1,10 @@
-import Map from "@/components/Map"
+'use client'
+
+import dynamic from "next/dynamic"
+const Map  = dynamic(
+    ()=> import('@/components/Map'),
+    {ssr:false}
+)
 export default function MapPage () {
     return(
         <div className="h-screen flex flex-col items-center">
