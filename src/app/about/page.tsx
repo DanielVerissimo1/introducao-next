ff8d0c3d7f97adcf9fd2b4c882074c9073668fdf
DanielVerissimo1 <danielSantoss1300@gmail.com>
2025-08-04 20:56:16 -0300

    feat: Atividade de props

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,4 +1,5 @@
 import Button from "@/components/Button"
+import { CardDemo } from "@/components/CardDemo"
 export default function About(){
 
     return(
@@ -6,7 +7,34 @@ export default function About(){
             <h1 className="text-2x1 ">Sobre nós</h1>
             <p>Informações sobre a empresa </p>
             <Button label="clique-me"/>
+            <div className="flex gap-5 items-center justify-center mt-4">
+                <CardDemo
+                title="Clube da Luta"
+                description="Primeiro você tem que se entregar, primeiro você tem que saber não temer, saber que um dia você vai morrer. "
+                imageUrl="https://images.unsplash.com/photo-1476842634003-7dcca8f832de?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80"
+                
+                imageUrlHover="https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExNWlodTF3MjJ3NnJiY3Rlc2J0ZmE0c28yeWoxc3gxY2VtZzA5ejF1NSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/syEfLvksYQnmM/giphy.gif"/>
+                <CardDemo
+                title="Clube da Luta"
+                description="Primeiro você tem que se entregar, primeiro você tem que saber não temer, saber que um dia você vai morrer. "
+                imageUrl="https://images.unsplash.com/photo-1476842634003-7dcca8f832de?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80"
+                
+                imageUrlHover="https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExNWlodTF3MjJ3NnJiY3Rlc2J0ZmE0c28yeWoxc3gxY2VtZzA5ejF1NSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/syEfLvksYQnmM/giphy.gif"/>
+                <CardDemo
+                title="Clube da Luta"
+                description="Primeiro você tem que se entregar, primeiro você tem que saber não temer, saber que um dia você vai morrer. "
+                imageUrl="https://images.unsplash.com/photo-1476842634003-7dcca8f832de?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80"
+                
+                imageUrlHover="https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExNWlodTF3MjJ3NnJiY3Rlc2J0ZmE0c28yeWoxc3gxY2VtZzA5ejF1NSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/syEfLvksYQnmM/giphy.gif"/>
 
+                <CardDemo
+                title="Clube da Luta"
+                description="Primeiro você tem que se entregar, primeiro você tem que saber não temer, saber que um dia você vai morrer. "
+                imageUrl="https://images.unsplash.com/photo-1476842634003-7dcca8f832de?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80"
+                
+                imageUrlHover="https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExNWlodTF3MjJ3NnJiY3Rlc2J0ZmE0c28yeWoxc3gxY2VtZzA5ejF1NSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/syEfLvksYQnmM/giphy.gif"/>
+            </div>
+        
         </div>
     )
 }
---
+++ src/components/CardDemo.tsx
@@ -0,0 +1,47 @@
+// Crie um componente Card que receba title, description e imageUrl como props e exiba essas informações no componente. Utilize esse componente na página de sua escolha.Faça a atividade em uma branch separada, abra um Pull Request e me envie o link para eu ver as alterações commitadas.
+"use client";
+import { cn } from "@/lib/utils";
+
+interface CardProps {
+    title: string;
+    description: string;
+    imageUrl: string;
+    imageUrlHover: string;
+}
+
+export function CardDemo({ title, description,  imageUrl, imageUrlHover}: CardProps) {
+  return (
+    <div className="max-w-xs w-full mt-1.5">
+      <div
+        className={cn(
+          "group w-full cursor-pointer overflow-hidden relative card h-96 rounded-md shadow-xl mx-auto flex flex-col justify-end p-4 border border-transparent dark:border-neutral-800",
+
+          `bg-[url(${imageUrl})] bg-cover`,
+            // o before faz uma pré rederizaçao da imagem de hover
+          `before:bg-[url(${imageUrlHover})] before:fixed before:inset-0 before:opacity-0 before:z-[-1]`,
+            // renderiza a imagem de hover 
+          `hover:bg-[url(${imageUrlHover})]`,
+          "hover:after:content-[''] hover:after:absolute hover:after:inset-0 hover:after:bg-black hover:after:opacity-50",
+          "transition-all duration-500"
+        )}
+        /* className={cn(
+          "group w-full cursor-pointer overflow-hidden relative card h-96 rounded-md shadow-xl mx-auto flex flex-col justify-end p-4 border border-transparent dark:border-neutral-800",
+          "bg-[url(https://images.unsplash.com/photo-1476842634003-7dcca8f832de?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80)] bg-cover",
+          // Preload hover image by setting it in a pseudo-element
+          "before:bg-[url(https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExNWlodTF3MjJ3NnJiY3Rlc2J0ZmE0c28yeWoxc3gxY2VtZzA5ejF1NSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/syEfLvksYQnmM/giphy.gif)] before:fixed before:inset-0 before:opacity-0 before:z-[-1]",
+          "hover:bg-[url(https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExNWlodTF3MjJ3NnJiY3Rlc2J0ZmE0c28yeWoxc3gxY2VtZzA5ejF1NSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/syEfLvksYQnmM/giphy.gif)]",
+          "hover:after:content-[''] hover:after:absolute hover:after:inset-0 hover:after:bg-black hover:after:opacity-50",
+          "transition-all duration-500"*/
+      >
+        <div className="text relative z-50">
+          <h1 className="font-bold text-xl md:text-3xl text-gray-50 relative">  
+            {title}
+          </h1>
+          <p className="font-normal text-base break-words text-gray-50 relative my-4">
+            {description}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
