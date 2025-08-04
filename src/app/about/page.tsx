ebd03ec82943b0e1595c002f58402df568609b23
DanielVerissimo1 <danielSantoss1300@gmail.com>
2025-08-04 19:27:30 -0300

    revisando conceitos de reat

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,10 +1,12 @@
-
+import Button from "@/components/Button"
 export default function About(){
 
     return(
         <div>
-            <h1>Sobre nós</h1>
-            <p>Informações sobre a empresa ...</p>
+            <h1 className="text-2x1 ">Sobre nós</h1>
+            <p>Informações sobre a empresa </p>
+            <Button label="clique-me"/>
+
         </div>
     )
 }
---
+++ src/components/Button.tsx
@@ -0,0 +1,14 @@
+
+type ButtonProps = {
+    label: string; // ? a propriedade label nao é obrigatória
+}
+
+export default function Button({ label }: ButtonProps) {  // eu obrigo que o componente receba uma propriedade chamada label, que é  ButtonProps. any -> ta d
+    return (
+        <button  className="px-4 py-2 mx-1.5 bg-blue-400 text-white cursor-pointer rounded hover:bg-blue-500 transition-colors">
+            {label}
+        </button>
+    );
+    
+}
+
