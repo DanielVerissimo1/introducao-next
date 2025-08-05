07c6f79cef7f2c934cc0daf7d52617c974e36c54
DanielVerissimo1 <danielSantoss1300@gmail.com>
2025-08-04 22:52:02 -0300

    feat: realizando atividade de props

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,40 +1,34 @@
 import Button from "@/components/Button"
-import { CardDemo } from "@/components/CardDemo"
+import { ThreeDCardDemo } from "@/components/ThreeDCardDemo"
 export default function About(){
-
+    
     return(
         <div>
             <h1 className="text-2x1 ">Sobre nós</h1>
             <p>Informações sobre a empresa </p>
             <Button label="clique-me"/>
-            <div className="flex gap-5 items-center justify-center mt-4">
-                <CardDemo
-                title="Clube da Luta"
-                description="Primeiro você tem que se entregar, primeiro você tem que saber não temer, saber que um dia você vai morrer. "
-                imageUrl="https://images.unsplash.com/photo-1476842634003-7dcca8f832de?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80"
-                
-                imageUrlHover="https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExNWlodTF3MjJ3NnJiY3Rlc2J0ZmE0c28yeWoxc3gxY2VtZzA5ejF1NSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/syEfLvksYQnmM/giphy.gif"/>
-                <CardDemo
-                title="Clube da Luta"
-                description="Primeiro você tem que se entregar, primeiro você tem que saber não temer, saber que um dia você vai morrer. "
-                imageUrl="https://images.unsplash.com/photo-1476842634003-7dcca8f832de?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80"
-                
-                imageUrlHover="https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExNWlodTF3MjJ3NnJiY3Rlc2J0ZmE0c28yeWoxc3gxY2VtZzA5ejF1NSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/syEfLvksYQnmM/giphy.gif"/>
-                <CardDemo
-                title="Clube da Luta"
-                description="Primeiro você tem que se entregar, primeiro você tem que saber não temer, saber que um dia você vai morrer. "
-                imageUrl="https://images.unsplash.com/photo-1476842634003-7dcca8f832de?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80"
-                
-                imageUrlHover="https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExNWlodTF3MjJ3NnJiY3Rlc2J0ZmE0c28yeWoxc3gxY2VtZzA5ejF1NSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/syEfLvksYQnmM/giphy.gif"/>
 
-                <CardDemo
-                title="Clube da Luta"
-                description="Primeiro você tem que se entregar, primeiro você tem que saber não temer, saber que um dia você vai morrer. "
-                imageUrl="https://images.unsplash.com/photo-1476842634003-7dcca8f832de?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80"
-                
-                imageUrlHover="https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExNWlodTF3MjJ3NnJiY3Rlc2J0ZmE0c28yeWoxc3gxY2VtZzA5ejF1NSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/syEfLvksYQnmM/giphy.gif"/>
+            <div className="flex-col items-center justify-center ">
+                <h1 className="text-center font-bold text-xl md:text-4xl text-gray-700 relative">Cards</h1>
+                <div className="flex gap-5 items-center justify-center">
+                    <ThreeDCardDemo
+                    title="Clube da Luta"
+                    description="Primeiro você tem que se entregar, primeiro você tem que saber não temer, saber que um dia você vai morrer. "
+                    imageUrl="https://disney.images.edge.bamgrid.com/ripcut-delivery/v2/variant/disney/fa086dc9-2c08-4c47-9391-66c59aba3077/compose?aspectRatio=1.78&format=webp&width=1200"
+                    />
+                    <ThreeDCardDemo
+                    title="Clube da Luta"
+                    description="Primeiro você tem que se entregar, primeiro você tem que saber não temer, saber que um dia você vai morrer. "
+                    imageUrl="https://disney.images.edge.bamgrid.com/ripcut-delivery/v2/variant/disney/fa086dc9-2c08-4c47-9391-66c59aba3077/compose?aspectRatio=1.78&format=webp&width=1200"
+                    />
+                    <ThreeDCardDemo
+                    title="Clube da Luta"
+                    description="Primeiro você tem que se entregar, primeiro você tem que saber não temer, saber que um dia você vai morrer. "
+                    imageUrl="https://disney.images.edge.bamgrid.com/ripcut-delivery/v2/variant/disney/fa086dc9-2c08-4c47-9391-66c59aba3077/compose?aspectRatio=1.78&format=webp&width=1200"
+                    />
+                   
+                </div>
             </div>
-        
         </div>
     )
 }
