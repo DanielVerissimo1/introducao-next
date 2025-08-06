1214f3bd0e9348c5041f1d902efc830040ff2fba
DanielVerissimo1 <danielSantoss1300@gmail.com>
2025-08-06 18:43:20 -0300

    feat: aticidade5

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,17 @@
+'use client'
 import Button from "@/components/Button"
 import { ThreeDCardDemo } from "@/components/ThreeDCardDemo"
+import CommentForm from '@/components/CommentForm';
+import CommentList from '@/components/CommentList';
+import { useState } from "react"
+
+interface CommentData {
+  id: string;
+  author: string;
+  text: string;
+  date: string;
+}
+
 export default function About(){
     const dadosFicticios = [
           {
@@ -47,6 +59,19 @@ export default function About(){
              
         ]
 
+    const [comments, setComments] = useState<CommentData[]>([]);
+
+    const handleAddComment = (author: string, text: string) => {
+        const newComment: CommentData = {
+        id: Date.now().toString(),
+        author,
+        text,
+        date: new Date().toLocaleString('pt-BR'),
+        };
+
+    setComments([newComment, ...comments]);
+  };
+    
     return(
         <div className="bg-gray-100">
             <h1 className="text-2x1 ">Sobre nós</h1>
@@ -92,6 +117,11 @@ export default function About(){
                     </div>
                 </div>
             </div>
+            </div>
+            <div className="max-w-2xl mx-auto p-4">
+                <h1 className="text-2xl font-bold mb-4">Comentários</h1>
+                <CommentForm onAddComment={handleAddComment} />
+                <CommentList comments={comments} />
             </div>
 
         </div>
